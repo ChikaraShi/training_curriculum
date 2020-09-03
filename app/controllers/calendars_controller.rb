bd62bad0e01c2157524da3924315ec91c53d9316
ChikaraShi <chikara.2000.082@gmail.com>
2020-09-03 19:53:11 +0900

    fix1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -24,8 +23,7 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
+ 
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
@@ -35,12 +33,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      # wday_num = Date.today.wday + x を利用して添字となる数値を得る
 
-      #もしもwday_numが7以上であれば、7を引く
-      # if wdays >= 7
-      #   wday_num = wday_num - 7
-      # end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: wdays[(@todays_date+x).wday], plans: today_plans }
       @week_days.push(days)
     end
